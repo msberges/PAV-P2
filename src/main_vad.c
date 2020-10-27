3871f54e62375a59f5af3eaaba17967052cd5b96
Rubén <ruben-379@hotmail.com>
2020-10-27 14:58:24 +0100

    Ficheros originales de la práctica

--- src/main_vad.c
+++ src/main_vad.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
   float *buffer, *buffer_zeros;
   int frame_size;         /* in samples */
   float frame_duration;   /* in seconds */
-  unsigned int t, last_t; /* in frames */
+  unsigned int t, last_t, maybe_t; /* in frames */
 
   char	*input_wav, *output_vad, *output_wav;
 
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
   frame_duration = (float) frame_size/ (float) sf_info.samplerate;
   last_state = ST_UNDEF;
 
-  for (t = last_t = 0; ; t++) { /* For each frame ... */
+  for (t = last_t = maybe_t = 0; ; t++) { /* For each frame ... */
     /* End loop when file has finished (or there is an error) */
     if  ((n_read = sf_read_float(sndfile_in, buffer, frame_size)) != frame_size) break;
 
@@ -86,12 +86,39 @@ int main(int argc, char *argv[]) {
 
     /* TODO: print only SILENCE and VOICE labels */
     /* As it is, it prints UNDEF segments but is should be merge to the proper value */
+    
+    /* Original:
     if (state != last_state) {
       if (t != last_t)
         fprintf(vadfile, "%.5f\t%.5f\t%s\n", last_t * frame_duration, t * frame_duration, state2str(last_state));
       last_state = state;
       last_t = t;
     }
+    */
+
+    /*Test nostre:*/ 
+    
+    if (state != last_state) { /*Canvia l'estat?*/
+      if (t != last_t && t != maybe_t){ /*Comprobem que no estem en el mateix instant per seguretat*/
+
+        if (state == ST_MAYBE_VOICE || state == ST_MAYBE_SILENCE){
+          maybe_t = t;
+        } else if (state == ST_VOICE && last_state == ST_MAYBE_VOICE) { /*Confirmem el canvi a Veu*/
+            /*Ja coneixem tot l'interval de silenci -> l'escribim:*/
+            fprintf(vadfile, "%.5f\t%.5f\t%s\n", last_t * frame_duration, maybe_t * frame_duration, state2str(ST_SILENCE)); 
+            /*Actualitzem dades:*/
+            last_t = maybe_t;
+        } else if (state == ST_SILENCE && last_state == ST_MAYBE_SILENCE) { /*Confirmem el canvi a silenci*/
+            /*Ja coneixem tot l'interval de veu -> l'escribim:*/
+            fprintf(vadfile, "%.5f\t%.5f\t%s\n", last_t * frame_duration, maybe_t * frame_duration, state2str(ST_VOICE)); 
+            /*Actualitzem dades:*/
+            last_t = maybe_t;
+        }
+        last_state = state;
+      }
+    }
+
+    /*Fi test nostre*/
 
     if (sndfile_out != 0) {
       /* TODO: go back and write zeros in silence segments */
@@ -100,8 +127,15 @@ int main(int argc, char *argv[]) {
 
   state = vad_close(vad_data);
   /* TODO: what do you want to print, for last frames? */
-  if (t != last_t)
+  if (t != last_t){
+    if(state == ST_MAYBE_VOICE){
+      state = ST_VOICE;
+    } else if(state == ST_MAYBE_SILENCE){
+      state = ST_SILENCE;
+    } 
+    
     fprintf(vadfile, "%.5f\t%.5f\t%s\n", last_t * frame_duration, t * frame_duration + n_read / (float) sf_info.samplerate, state2str(state));
+  }
 
   /* clean up: free memory, close open files */
   free(buffer);
--- src/vad.c
+++ src/vad.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "vad.h"
+#include "pav_analysis.h"
 
 const float FRAME_TIME = 10.0F; /* in ms. */
 
@@ -13,7 +14,7 @@ const float FRAME_TIME = 10.0F; /* in ms. */
  */
 
 const char *state_str[] = {
-  "UNDEF", "S", "V", "INIT"
+  "UNDEF", "S", "V", "INIT", "MV","MS"
 };
 
 const char *state2str(VAD_STATE st) {
@@ -31,7 +32,7 @@ typedef struct {
  * TODO: Delete and use your own features!
  */
 
-Features compute_features(const float *x, int N) {
+Features compute_features(const float *x, int N, float fm) {
   /*
    * Input: x[i] : i=0 .... N-1 
    * Ouput: computed features
@@ -42,7 +43,10 @@ Features compute_features(const float *x, int N) {
    * For the moment, compute random value between 0 and 1 
    */
   Features feat;
-  feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  feat.p = compute_power(x,N);
+  feat.am = compute_am(x,N);
+  feat.zcr = compute_zcr(x,N,fm);
+  //feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
   return feat;
 }
 
@@ -55,6 +59,7 @@ VAD_DATA * vad_open(float rate) {
   vad_data->state = ST_INIT;
   vad_data->sampling_rate = rate;
   vad_data->frame_length = rate * FRAME_TIME * 1e-3;
+  vad_data->frame_cnt = 0;
   return vad_data;
 }
 
@@ -84,22 +89,74 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
    * program finite state automaton, define conditions, etc.
    */
 
-  Features f = compute_features(x, vad_data->frame_length);
+  Features f = compute_features(x, vad_data->frame_length, vad_data->sampling_rate);
   vad_data->last_feature = f.p; /* save feature, in case you want to show */
 
   switch (vad_data->state) {
   case ST_INIT:
+
+    /*
     vad_data->state = ST_SILENCE;
+    vad_data->zcr_0 = 7000;
+    vad_data->min_voice = 6;
+    vad_data->min_silence = 6;
+    vad_data->maybe_cnt = 0;
+    vad_data->alfa1 = 2;//2
+    vad_data->alfa2 = 5;//5
+    vad_data->k0 = f.p;
+    vad_data->k1 = vad_data->k0 + vad_data->alfa1;
+    vad_data->k2 = vad_data->k1 + vad_data->alfa2;
+    */
+
+    if (vad_data->frame_cnt < 10) {
+      vad_data->k0 += pow(10,f.p/10);
+      vad_data->frame_cnt++;
+      if(vad_data->frame_cnt == 10){
+        vad_data->k0 = 10*log10(vad_data->k0/vad_data->frame_cnt);
+        vad_data->alfa1 = 1.7;
+        vad_data->alfa2 = 4.5;
+        vad_data->k1 = vad_data->k0 + vad_data->alfa1;
+        vad_data->k2 = vad_data->k1 + vad_data->alfa2;
+        vad_data->min_voice = 7; 
+        vad_data->min_silence = 8;
+        vad_data->maybe_cnt = 0;
+        vad_data->zcr_0 = 3000;
+        vad_data->state = ST_SILENCE;
+      }
+    }
+
     break;
 
   case ST_SILENCE:
-    if (f.p > 0.95)
-      vad_data->state = ST_VOICE;
+    if (f.p > vad_data->k1 || f.zcr > vad_data->zcr_0) 
+      vad_data->state = ST_MAYBE_VOICE;
     break;
 
   case ST_VOICE:
-    if (f.p < 0.01)
+    if (f.p < vad_data->k2 && f.zcr < vad_data->zcr_0) 
+      vad_data->state = ST_MAYBE_SILENCE;
+    break;
+
+  case ST_MAYBE_VOICE:
+    vad_data->maybe_cnt++;
+    if ((f.p > vad_data->k2 || f.zcr > vad_data->zcr_0) && vad_data->maybe_cnt >= vad_data->min_voice) { 
+      vad_data->state = ST_VOICE;
+      vad_data->maybe_cnt = 0;
+    } else if (f.p < vad_data->k1 && f.zcr < vad_data->zcr_0) { 
       vad_data->state = ST_SILENCE;
+      vad_data->maybe_cnt = 0;
+    } 
+    break;
+
+  case ST_MAYBE_SILENCE:
+    vad_data->maybe_cnt++;
+    if (f.p < vad_data->k1 && f.zcr < vad_data->zcr_0 && vad_data->maybe_cnt >= vad_data->min_silence) { 
+      vad_data->state = ST_SILENCE;
+      vad_data->maybe_cnt = 0;
+    } else if (f.p > vad_data->k2 || f.zcr > vad_data->zcr_0) { 
+      vad_data->state = ST_VOICE;
+      vad_data->maybe_cnt = 0;
+    }
     break;
 
   case ST_UNDEF:
@@ -107,7 +164,9 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
   }
 
   if (vad_data->state == ST_SILENCE ||
-      vad_data->state == ST_VOICE)
+      vad_data->state == ST_VOICE ||
+      vad_data->state == ST_MAYBE_VOICE ||
+      vad_data->state == ST_MAYBE_SILENCE )
     return vad_data->state;
   else
     return ST_UNDEF;
